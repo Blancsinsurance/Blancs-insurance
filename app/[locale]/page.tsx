b7f5ee792bcc10ff88423c5d8d0af29c0a6441ad
IO31-WEB <ISAIAHOLIVER95@YAHOO.COM>
2026-09-10 09:32:41 -0400

    Add AppDownload component to HomePage

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,6 +2,7 @@ import { setRequestLocale } from "next-intl/server";
 import Hero from "@/components/Hero";
 import ServicesGrid from "@/components/ServicesGrid";
 import AgentsGrid from "@/components/AgentsGrid";
+import AppDownload from "@/components/AppDownload";
 
 export default function HomePage({
   params: { locale },
@@ -14,6 +15,7 @@ export default function HomePage({
       <Hero locale={locale} />
       <ServicesGrid />
       <AgentsGrid locale={locale} />
+      <AppDownload />
     </>
   );
 }
